9041d153207afc05c391aaaf47e50069858c29f0
EWU Web.Lab <pmanikoth@ewu.edu>
2022-10-13 10:57:07 -0700

    Production
    * Setup production site
    * Added confetti JS
    * Tested locally

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // My Scripts
-
 console.log('Page Loaded');
+
+// Run it
+let confetti = new Confetti("confetti");
+// let confetti2 = new Confetti("confetti2");
+
+// Options
+confetti.setSize(6);
+confetti.destroyTarget(false);
